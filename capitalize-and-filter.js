4a82417385c76758b29c57de0ab24896a92014d5
‘Katie <ktschr11@gmail.com>
2021-10-25 17:50:36 -0700

    unable to get tests passing with for loop

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,5 +1,8 @@
-const capitalizeAndFilter = () => {
-
-}
-
+const capitalizeAndFilter = (arr) => {
+  const newArr = [];
+  arr.forEach (item => {
+    newArr.push(item.toUpperCase());
+  });
+  return newArr;
+};
 module.exports = capitalizeAndFilter;
