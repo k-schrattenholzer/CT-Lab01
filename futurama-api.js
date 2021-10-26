dfd46502628df727c4bc0e8ca4b05f3b2295ec30
‘Katie <ktschr11@gmail.com>
2021-10-26 10:39:27 -0700

    test passing using async/await format

--- futurama-api.js
+++ futurama-api.js
@@ -1,19 +1,35 @@
 const fetch = require('cross-fetch');
 
+// const getQuote = async () => {
+//     return fetch('http://futuramaapi.herokuapp.com')
+//   const res = await ;
+//   const body = await res.json();
+
+//   const randomIndex = Math.random() * (300 - 1) + 1;
+
+//   const mungedRes = {
+//     name: body[randomIndex].character,
+//     text: body[randomIndex].quote,
+//     image: body[randomIndex].image
+//   };
+  
+//   return mungedRes; 
+// };
+
 const getQuote = async () => {
-  const res = await fetch('http://futuramaapi.herokuapp.com');
+  const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes');
+
   const body = await res.json();
 
-  //   const mungedRes = {
-  //     name: res.,
-  //     text: expect.any(String),
-  //     image: expect.any(String)
-  //   };
+  const singleQuote = body[Math.floor((Math.random() * 1000) + 1)];
 
-  //   return mungedRes;
-
-  console.log(body);
+  const mungedRes = {
+    name: singleQuote.character,
+    text: singleQuote.quote,
+    image: singleQuote.image
+  };
+  
+  return mungedRes; 
 };
 
 module.exports = getQuote;
-
