c1357bddf3a2d36023af6496c510eeb314b74fee
‘Katie <ktschr11@gmail.com>
2021-10-25 17:31:03 -0700

    added test to ensure function is immutable

--- copy-and-push.test.js
+++ copy-and-push.test.js
@@ -12,4 +12,14 @@ describe('copyAndPush', () => {
     expect(copyAndPush(arr, newItem))
       .toEqual(expect.arrayContaining([newItem]));  
   }); 
+  it('should leave the original array unchanged', () => {
+
+    const arr = ['miss vanjie', 'hair-ties', 'my big pink bed', 'snuggles (but not too much)'];
+
+    const newItem = 'watching cat-tv out the window';
+
+    const updatedArr = copyAndPush(arr, newItem);
+
+    expect(arr).not.toEqual(updatedArr);  
+  }); 
 });
