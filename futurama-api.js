ef1470c25da123491b62c84ae91882f5f11c3fb3
‘Katie <ktschr11@gmail.com>
2021-10-26 10:45:19 -0700

    updated function to choose a random index less than 20

--- futurama-api.js
+++ futurama-api.js
@@ -1,27 +1,12 @@
 const fetch = require('cross-fetch');
 
-// const getQuote = async () => {
-//     return fetch('http://futuramaapi.herokuapp.com')
-//   const res = await ;
-//   const body = await res.json();
-
-//   const randomIndex = Math.random() * (300 - 1) + 1;
-
-//   const mungedRes = {
-//     name: body[randomIndex].character,
-//     text: body[randomIndex].quote,
-//     image: body[randomIndex].image
-//   };
-  
-//   return mungedRes; 
-// };
-
 const getQuote = async () => {
   const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes');
 
   const body = await res.json();
 
-  const singleQuote = body[Math.floor((Math.random() * 1000) + 1)];
+  console.log(body);
+  const singleQuote = body[Math.ceil((Math.random() * 20) + 1)];
 
   const mungedRes = {
     name: singleQuote.character,
