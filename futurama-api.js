0bbccb0e806af60e6d11c3aaae381a5f650f9310
‘Katie <ktschr11@gmail.com>
2021-10-26 10:20:54 -0700

    fixed test

--- futurama-api.js
+++ futurama-api.js
@@ -1,7 +1,18 @@
 const fetch = require('cross-fetch');
 
 const getQuote = async () => {
+  const res = await fetch('http://futuramaapi.herokuapp.com');
+  const body = await res.json();
 
+  //   const mungedRes = {
+  //     name: res.,
+  //     text: expect.any(String),
+  //     image: expect.any(String)
+  //   };
+
+  //   return mungedRes;
+
+  console.log(body);
 };
 
 module.exports = getQuote;
--- futurama-api.test.js
+++ futurama-api.test.js
@@ -1,11 +1,13 @@
 const getQuote = require('./futurama-api.js');
 
-describe('should fetch a single quote from the futurama API', async () => {
-  const quote = await getQuote();
+describe('getQuote', () => {
+  it('should fetch a single quote from the futurama API', async () => {
+    const quote = await getQuote();
     
-  expect(quote).toEqual({
-    name: expect.any(String),
-    text: expect.any(String),
-    image: expect.any(String)
+    expect(quote).toEqual({
+      name: expect.any(String),
+      text: expect.any(String),
+      image: expect.any(String)
+    });
   });
 });
